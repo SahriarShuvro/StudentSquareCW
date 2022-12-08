0773f8f90b3273fde2fc4484994dfddd88a7e9a4
SahriarShuvro <80584229+SahriarShuvro@users.noreply.github.com>
2022-12-09 01:02:58 +0600

    Add files via upload

--- js/blogSlider.js
+++ js/blogSlider.js
@@ -1,46 +1,9 @@
-// $(".blogBody").slick({
-//     dots: true,
-//     infinite: true,
-//     speed: 300,
-//     slidesToShow: 4,
-//     slidesToScroll: 4,
-//     prevArrow: false,
-//     nextArrow: false,
-//     responsive: [{
-//             breakpoint: 1024,
-//             settings: {
-//                 slidesToShow: 3,
-//                 slidesToScroll: 3,
-//                 infinite: true,
-//                 dots: true,
-//             },
-//         },
-//         {
-//             breakpoint: 600,
-//             settings: {
-//                 slidesToShow: 2,
-//                 slidesToScroll: 2,
-//             },
-//         },
-//         {
-//             breakpoint: 480,
-//             settings: {
-//                 slidesToShow: 1,
-//                 slidesToScroll: 1,
-//             },
-//         },
-//     ],
-// });
-
-// let blogNextBTN = document.querySelector('.slick-dots');
-// blogNextBTN.classList.add('blogNextBTN');
 $(".blogBody").slick({
     centerMode: true,
     centerPadding: "60px",
     slidesToShow: 4,
-    prevArrow: false,
-    nextArrow: false,
-    dots: true,
+    prevArrow: true,
+    nextArrow: true,
     responsive: [{
             breakpoint: 900,
             settings: {
